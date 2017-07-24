bc4b49855611d8849c993d81b6e6f5dcbf8def3f
Tom Beck <tom@tastylounge.com>
2017-07-24 10:53:55 -0700

    updated gulp tasks to include jekyll build and browsersync

--- gulp/config.js
+++ gulp/config.js
@@ -1,18 +1,72 @@
-var clientname        = 'amara';
-var src               = 'src';
-var srcAssets         = 'src/_assets';
-var bowerAssets       = 'bower_components';
+// var src               = 'src';
+// var srcAssets         = 'src/_assets';
+// var bowerAssets       = 'bower_components';
 
+
+// module.exports = {
+//   bower: {
+//     debugging: true,
+//     includeDev: true,
+//     dest: src + '/libs',
+//     paths: {
+//       bowerDirectory: bowerAssets,
+//       bowerrc: './.bowerrc',
+//       bowerJson: './bower.json'
+//     }
+//   }
+// };
+
+var src               = 'src';
+var build             = 'build';
+var development       = 'build/development';
+var production        = 'build/production';
+var srcAssets         = 'src/_assets';
+var developmentAssets = 'build/assets';
+var productionAssets  = 'build/production/assets';
 
 module.exports = {
-  bower: {
-    debugging: true,
-    includeDev: true,
-    dest: src + '/libs',
-    paths: {
-      bowerDirectory: bowerAssets,
-      bowerrc: './.bowerrc',
-      bowerJson: './bower.json'
+  browsersync: {
+    development: {
+      server: {
+        baseDir: [development, build, src]
+      },
+      port: 9999,
+      files: [
+        developmentAssets + '/css/*.css',
+        developmentAssets + '/js/*.js',
+        developmentAssets + '/images/**',
+        developmentAssets + '/fonts/*'
+      ]
     }
+  },
+  delete: {
+    src: [developmentAssets]
+  },
+  jekyll: {
+    development: {
+      src:    src,
+      dest:   development,
+      config: '_config.yml'
+    }
+  },
+  watch: {
+    jekyll: [
+      '_config.yml',
+      '_config.build.yml',
+      'stopwords.txt',
+      src + '/_data/**/*.{json,yml,csv}',
+      src + '/_includes/**/*.{html,xml}',
+      src + '/_layouts/*.html',
+      src + '/_locales/*.yml',
+      src + '/_plugins/*.rb',
+      src + '/_posts/*.{markdown,md}',
+      src + '/**/*.{html,markdown,md,yml,json,txt,xml}',
+      src + '/*'
+    ],
+    styles:  srcAssets + '/styles/**/*.css',
+    scripts: srcAssets + '/javascripts/**/*.js',
+    images:  srcAssets + '/images/**/*',
+    sprites: srcAssets + '/images/**/*.png',
+    svg:     'vectors/*.svg'
   }
 };
