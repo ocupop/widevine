f7fda4ebbeb041c740505749d018986d41b0dbc4
Tom Beck <tom@tastylounge.com>
2017-09-11 09:48:48 -0700

    updated webfontkitloader and file structure for examples / sandbox

--- src/_assets/scripts/head.js
+++ src/_assets/scripts/head.js
@@ -1,1 +1,21 @@
 // require('modernizr');
+
+var WebFont = require('webfontloader');
+
+WebFont.load({
+  google: {
+    families: ['Open Sans']
+  },
+  // typekit: { id: '' },
+  loading: function() {
+    console.log("LOADING FONTS");
+  },
+  active: function() {
+    console.log("ACTIVE FONTS");
+  },
+  inactive: function() {},
+  fontloading: function(familyName, fvd) {},
+  fontactive: function(familyName, fvd) {},
+  fontinactive: function(familyName, fvd) {},
+  timeout: 2000 // Set the timeout to two seconds/
+});
