8208547f2046c3af7f545f024d881319618b04e8
Jon Burr <yojonathanwburr@gmail.com>
2018-03-23 07:23:08 -0700

    page scroll / anchors

--- src/_assets/scripts/main.js
+++ src/_assets/scripts/main.js
@@ -8,4 +8,13 @@ $(function() {
 $(document).ready(function() {
   //initiate tab collapse (if in use on this project)
   $('#my-tab').tabCollapse();
+
+  //scroll anchors
+  $(document).on('click', 'a[href^="#"]', function (event) {
+      event.preventDefault();
+
+      $('html, body').animate({
+          scrollTop: $($.attr(this, 'href')).offset().top
+      }, 500);
+  });
 });
