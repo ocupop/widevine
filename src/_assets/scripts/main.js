0eef01665e51b386dac6e9635bfe2923124a8402
Jon Burr <yojonathanwburr@gmail.com>
2018-03-23 15:59:50 -0700

    add jquery-lazy and example

--- src/_assets/scripts/main.js
+++ src/_assets/scripts/main.js
@@ -1,8 +1,10 @@
 require('jquery');
+require('jquery-lazy');
 //var bootstrap = require('bootstrap');
 
 $(function() {
   console.log('jQuery loaded');
+  
 });
 
 $(document).ready(function() {
@@ -16,5 +18,27 @@ $(document).ready(function() {
       $('html, body').animate({
           scrollTop: $($.attr(this, 'href')).offset().top
       }, 500);
+  });
+
+  $("img.lazy").Lazy({
+    beforeLoad: function(element) {
+      window.console.log('before');
+        // called before an elements gets handled
+    },
+    afterLoad: function(element) {
+      window.console.log('after');
+        // called after an element was successfully handled
+    },
+    onError: function(element) {
+      window.console.log('error');
+        // called whenever an element could not be handled
+    },
+    onFinishedAll: function() {
+      window.console.log('finished');
+        // called once all elements was handled
+    },
+    threshold: -100
+
+
   });
 });
