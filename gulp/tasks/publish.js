c81f211c1be02e4242cc6ed343ac89bab4ba9775
Tom Beck <tom@tastylounge.com>
2018-05-15 22:05:46 -0700

    added baseline firebase deploy

--- gulp/tasks/publish.js
+++ gulp/tasks/publish.js
@@ -1,5 +1,6 @@
 const gulp    = require('gulp');
 const ghPages = require('gulp-gh-pages');
+const shell   = require('gulp-shell');
 const config  = require('../../project.config');
 
 // const log = require('fancy-log');
@@ -13,8 +14,17 @@ Object.keys(config.publish.tasks).forEach(function (key) {
 
 // Start browsersync task and then watch files for changes
 gulp.task('publish', publishTasks, function() {
+  // Publish to ghPages
   if(config.publish.github) {
     return gulp.src(config.publish.src + '/**/*')
       .pipe(ghPages());
   }
+
+  // Publish to Firebase
+  if(config.publish.firebase) {
+    return shell.task([
+      'firebase deploy'
+    ]);
+  }
+
 });
